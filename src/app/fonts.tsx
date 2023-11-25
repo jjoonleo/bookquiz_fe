44a68a7a071ef57a0f1a45e35c6c471d5b4994c2
Ejun Park <jjoonleo@gmail.com>
2023-11-25 14:15:07 +0900

    feat: add font NotoSansKR

---
+++ src/app/fonts.tsx
@@ -0,0 +1,11 @@
+import { Inter, Noto_Sans_KR } from 'next/font/google'
+ 
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
+export const noto_sans_kr = Noto_Sans_KR({
+  subsets: ['latin'],
+  display: 'swap',
+})
